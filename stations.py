eb99ff8e907c9cc4d1350dde18ef2ea22efcfdd7
Aakash Kattelu <Aakash.Kattelu11@qmail.cuny.edu>
2016-04-03 10:34:13 -0400

    final

--- stations.py
+++ stations.py
@@ -17,10 +17,12 @@ def getNearestTrainStation():
     Gets the nearest train station to a given point.
     args:
         point: the longitude/latitude point to locate around.
+        line (optional): specifies the train line
     returns:
         data for the nearest train station in json format.
     """
     point = request.args.get("point")
+    line = request.args.get("line") or None
     points = point.split(",")
     longitude = float(points[0][1:])
     latitude = float(points[1][:len(points[1])-1])
@@ -29,25 +31,21 @@ def getNearestTrainStation():
     for station in cursor.get("features"):
         coords = station.get("geometry").get("coordinates")
         if distance(longitude, latitude, coords[0], coords[1]) < minimum:
-            minimum = distance(longitude, latitude, coords[0], coords[1])
-            closest = station
+            if line is not None:
+                if line in station.get("properties").get("line"):
+                    minimum = distance(longitude, latitude, coords[0], coords[1])
+                    closest = station
+            else:
+                minimum = distance(longitude, latitude, coords[0], coords[1])
+                closest = station
+
     minimum=1000000
-    return jsonify(closest)
+    line = None
+    j = jsonify(closest)
+    closest = None
+    return j
 
 
-
-
-@app.route("/getNearestTrainStationByLine")
-def getNearestTrainStationByLine():
-    """
-    Gets the nearest train station to a given point based on subway lines.
-    args: 
-        point: the longitude/latitude point to locate around.
-        line: subway line to filter by.
-    returns:
-        data for the train station in json format.
-    """
-    pass
 
 @app.route("/getNearestXTrainStations")
 def getNearestXTrainStations():
@@ -59,10 +57,39 @@ def getNearestXTrainStations():
     returns:
         data for the train stations in json format.
     """
-    pass
+    point = request.args.get("point")
+    number = int(request.args.get("number"))
+    points = point.split(",")
+    longitude = float(points[0][1:])
+    latitude = float(points[1][:len(points[1])-1])
+    minimum = 1000000
+    closest = None
+
+    distances = [
+        distance(longitude,
+            latitude,
+            station.get("geometry").get("coordinates")[0],
+            station.get("geometry").get("coordinates")[1]
+            ) for station in cursor.get("features")
+    ]
 
 
 
+    distances.sort()
+    top_n = distances[0:number]
+    # # }},
+    output = []
+    for i in top_n:
+        for station in cursor.get("features"):
+            if i==distance(longitude,
+            latitude,
+            station.get("geometry").get("coordinates")[0],
+            station.get("geometry").get("coordinates")[1]
+            ):
+                output.append(station)
+
+
+    return jsonify({"top_" + str(number):output})
 if __name__ == '__main__':
     app.debug=True
     app.run("localhost", port=8000)
